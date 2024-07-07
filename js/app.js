54eb61bb0814222b48708e33b9b7b93c23cccabc
Orazhn <orazhanovkydyrbek@gmail.com>
2024-07-07 12:57:09 +0500

    fixed error 500

--- js/app.js
+++ js/app.js
@@ -218,60 +218,51 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Forms
 
-    const forms = document.querySelectorAll('form')
-
+    const forms = document.querySelectorAll('form');
     const message = {
-        loading: 'Загрузка',
-        success: 'Спасибо!',
-        failure: 'Что то пошло не так...'
-    }
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
 
     forms.forEach(item => {
-        postData(item)
-    })
+        postData(item);
+    });
 
-    function postData(form){
+    function postData(form) {
         form.addEventListener('submit', (e) => {
-            e.preventDefault()
+            e.preventDefault();
 
-            const timer = setTimeout(() => {
-                statusMessage.remove()
-                form.reset()
-            }, 3000)
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
+        
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
 
-            const statusMessage = document.createElement('div')
-            statusMessage.classList.add('status')
-            statusMessage.textContent = message.loading
-            form.append(statusMessage )
-
-            const request = new XMLHttpRequest()
-            request.open('POST', 'server.php')
-
-            request.setRequestHeader('Content-type', 'application/json')
-            const formData = new FormData(form)
-
-            const obj = {}
+            const object = {};
             formData.forEach(function(value, key){
-                obj[key] = value
-            })
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
 
-            const json = JSON.stringify(obj)
-
-            request.send(formData)
+            request.send(json);
 
             request.addEventListener('load', () => {
-                if (request.status == 200){
-                    console.log(request.response)
-                    statusMessage.textContent = message.success
-                    timer
-                    
-                    
-                }else {
-                    statusMessage.textContent = message.failure
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
                 }
-            })
-        })
+            });
+        });
     }
-    
 });
-
