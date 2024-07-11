833de1d6e6e90f2a317642b31474c10c3c253a44
Orazhn <orazhanovkydyrbek@gmail.com>
2024-07-11 12:08:24 +0500

    used fetch

--- js/app.js
+++ js/app.js
@@ -238,29 +238,34 @@ window.addEventListener('DOMContentLoaded', () => {
             `
             form.insertAdjacentElement('afterend', statusMessage)
         
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            const formData = new FormData(form);
+            
 
+            const formData = new FormData(form);
+            
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success)
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure)
-                }
-            });
+            fetch('server1.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data)
+                showThanksModal(message.success)
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure)
+            })
+            .finally(() => {
+                form.reset();
+            })
         });
     }
 
@@ -287,4 +292,19 @@ window.addEventListener('DOMContentLoaded', () => {
             hideModal()
        }, 4000)
     }
+
+
+
+    /*
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST', 
+        body: JSON.stringify({name: 'Alex'}),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
+      */
+
 });
